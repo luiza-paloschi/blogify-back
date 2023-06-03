fb4427f44cabe95d29004081eeb910ca994529b1
luiza-paloschi <luizapaloschi@gmail.com>
2023-06-03 20:18:35 -0300

    feat: user authentication

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import 'express-async-errors';
 import express, { Express } from 'express';
 import cors from 'cors';
 import { handleApplicationErrors } from './middlewares';
-import { usersRouter } from './routers';
+import { authenticationRouter, usersRouter } from './routers';
 import { loadEnv, connectDb, disconnectDB } from '@/config';
 
 loadEnv();
@@ -14,6 +14,7 @@ app
   .use(express.json())
   .get('/health', (_req, res) => res.send('OK!'))
   .use('/sign-up', usersRouter)
+  .use('/auth', authenticationRouter)
   .use(handleApplicationErrors);
 
 export function init(): Promise<Express> {
